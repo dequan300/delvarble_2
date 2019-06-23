809963eb9d5160222f2cd481f91ce662e6f83ef4
unknown <dequanchambers94@gmail.com>
2019-06-23 18:48:27 -0400

    update for class

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-var eventType = "casual";
-var tempFah = "more than 70 degrees Fahrenheit";
-var cloth1 = "no jacket";
+var cloth1 = "no need for a jacket";
 var cloth2 = "something comfy";
-var greeting = "whats the temperature like today ?";
-var weather = prompt(greeting);
-var results = (`since is will be ${tempFah} then ${cloth1} just be ${eventType} and wear ${cloth2}`);
-var results2 = "Are you sure ? It feels hotter than that!"
-if (weather >= 71) {
-    alert(results);
+var weather = "more than 70 degrees Fahrenheit"
+var casual = "should i wear something casual?";
+var temp = "will it be more than 70 degrees Fahrenheit ?";
+var eventype = prompt(casual);
+var tempFahr = prompt(temp);
+var results = (`since it will be ${weather} then ${cloth1} ill just wear ${cloth2}`);
+var wrong1 = ("you sure ? its pretty hot out")
+
+
+
+if (eventype === "yes") {
+    console.log("ok great")
+
+
 } else {
-    alert(results2)
+    console.log("Its only a picnic.")
+
+
+
+}
+if (tempFahr === "yes") {
+    console.log(results)
+} else {
+    console.log(wrong1)
 }
